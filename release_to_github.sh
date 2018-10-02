c318dbdf60f430f598542fcfd871d8e993f33626
Jay Rodgers <headmelted@gmail.com>
2018-10-02 08:50:40 +0100

    Update release_to_github.sh
    Cleaning up file and adding comments

--- release_to_github.sh
+++ release_to_github.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
+
+echo "Installing jq";
 sudo apt-get install -y jq;
-export GITHUB_RELEASE_TAG=$(date +%b-%y);
-JSON_DATA="{\"tag_name\": \"$GITHUB_RELEASE_TAG\",\"target_commitish\": \"master\",\"name\": \"$GITHUB_RELEASE_TAG\",\"body\": \"Build as of $GITHUB_RELEASE_TAG\",\"draft\": true, \"prerelease\": false}";
-echo "Pushing:";
-echo $JSON_DATA;
-echo "To:";
-CREATE_RELEASE_URL="https://api.github.com/repos/headmelted/prebootstrap/releases";
-github_response=$(curl --request POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_PAT" --data "$JSON_DATA" $CREATE_RELEASE_URL);
-GITHUB_RELEASE_ID=$(echo $github_response | jq -r ".id");
-assets_url_template=$(echo $github_response | jq -r ".upload_url");
-GITHUB_ASSETS_URL=${assets_url_template%"{?name,label}"};
-echo "Release ID: $GITHUB_RELEASE_ID";
-echo "Retrieved assets URL for later: $GITHUB_ASSETS_URL";
+
+github_release_tag=$(date +%b-%y);
+echo "Setting github_release_tag to $github_release_tag";
+
+echo "Creating draft release at Github"
+json_create_release="{\"tag_name\": \"$github_release_tag\",\"target_commitish\": \"master\",\"name\": \"$github_release_tag\",\"body\": \"Build as of $github_release_tag\",\"draft\": true, \"prerelease\": false}";
+create_release_url="https://api.github.com/repos/headmelted/prebootstrap/releases";
+github_response=$(curl --request POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_PAT" --data "$json_create_release" $create_release_url);
+
+echo "Parsing Github response"
+github_release_id=$(echo $github_response | jq -r ".id");
+github_assets_url_template=$(echo $github_response | jq -r ".upload_url");
+github_assets_url=${github_assets_url_template%"{?name,label}"};
+echo "Release ID: $github_release_id";
+echo "Retrieved assets URL for later: $github_assets_url";
+
 drop_folder="$SYSTEM_ARTIFACTSDIRECTORY/_headmelted.prebootstrap/drop";
-echo "Listing assets to upload:";
+echo "Assets to upload:";
 ls $drop_folder;
 for filename in $drop_folder/*.tar.gz
 do
-  echo "Uploading [$filename] to Github release [$GITHUB_RELEASE_ID]";
-  curl --request POST --data-binary "@$filename" -H "Authorization: token $GITHUB_PAT" -H "Content-Type: application/octet-stream" $GITHUB_ASSETS_URL?name=$(basename $filename);
+  echo "Uploading [$filename] to Github release [$github_release_id]";
+  curl --request POST --data-binary "@$filename" -H "Authorization: token $GITHUB_PAT" -H "Content-Type: application/octet-stream" $github_assets_url?name=$(basename $filename);
 done
-JSON_DATA="{\"tag_name\": \"$GITHUB_RELEASE_TAG\",\"target_commitish\": \"master\",\"name\": \"$GITHUB_RELEASE_TAG\",\"body\": \"Build as of $GITHUB_RELEASE_TAG\",\"draft\": false, \"prerelease\": false}";
-echo "Pushing:";
-echo $JSON_DATA;
-echo "To:";
-EDIT_RELEASE_URL="https://api.github.com/repos/headmelted/prebootstrap/releases/$GITHUB_RELEASE_ID";
-curl --request PATCH -H "Authorization: token $GITHUB_PAT" -H "Content-Type: application/json" --data "$JSON_DATA" "$EDIT_RELEASE_URL";
+
+echo "Marking release as final at Github"
+json_edit_release="{\"tag_name\": \"$github_release_tag\",\"target_commitish\": \"master\",\"name\": \"$github_release_tag\",\"body\": \"Build as of $github_release_tag\",\"draft\": false, \"prerelease\": false}";
+edit_release_url="https://api.github.com/repos/headmelted/prebootstrap/releases/$github_release_id";
+curl --request PATCH -H "Authorization: token $GITHUB_PAT" -H "Content-Type: application/json" --data "$json_edit_release" "$edit_release_url";
